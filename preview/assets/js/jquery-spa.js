3b71439dd9fcc900b410b84edef61518729f7f53
Vu Tran <baysao@gmail.com>
2026-08-15 21:01:09 +0700

    feat: add favicon assets and new preview HTML files with supporting scripts

--- preview/assets/js/jquery-spa.js
+++ preview/assets/js/jquery-spa.js
@@ -3,44 +3,83 @@ Jquery SPA without hash example. A simple code!
 Version: 0.1.0.
 Written by: Sedem stickx <sedemdatsa69@gmail.com>
 */
+function loadScripts(urls, length, success) {
+   if (length > 0) {
+      script = document.createElement("script");
+      script.src = urls[length - 1];
+      console.log();
+      script.onload = function () {
+         console.log('%c Script: ' + urls[length - 1] + ' loaded!', 'color: #4CAF50');
+         loadScripts(urls, length - 1, success);
+      };
+      document.getElementsByTagName("head")[0].appendChild(script);
+   }
+   else {
+      if (success) {
+         success();
+      }
+   }
+}
+function _load_scripts() {
+   /* Write links sorted from last one to first to load */
+   /* Here, jquery will be loaded first, then materialize and then wow libray. */
+   urls = ['assets/js/main.js'];
 
+   loadScripts(urls, urls.length, function () {
+      /* Codes inside of here will be executed after js files load */
+
+   });
+}
 //deprecated method but is very useful in showing type of page load action a user has taken.
 //For testing purposes.
 console.log(performance.navigation.type);
-
-//when user clicks on any anchor tag with a data-link attribute fire this event.
-$(document).on('click','[data-link]', function (e) {
-
-   e.preventDefault();//prevent anchor click default behaviour.
-
-   var page = $(this).attr('href');//get url from clicked link.
-
-   var routes = page.substring(0,page.lastIndexOf('.'));//remove file extension that shows up in the url bar.
-
-   window.history.pushState(null,null,routes);//assign new url to address bar and add page in browser history without reloading the page.
-      
-   console.log("Ajax loaded: "+page);
-
-   $.get(page, function (pageContent) {//return selected page content trough ajax.
-      $("#container").html(pageContent);//load content into main div
-   });
+$.get('part/footergif.html', function (pageContent) {//return selected page content trough ajax.
+   $("#footergif").html(pageContent);//load content into main div
 });
+function _load_default() {
+   //when user clicks on any anchor tag with a data-link attribute fire this event.
+   $(document).off().on('click', '[data-link]', function (e) {
+      console.log("test");
+      e.preventDefault();//prevent anchor click default behaviour.
 
- 
+      var routes = $(this).attr('href');//get url from clicked link. 
+      var page = 'part/' + (routes != '' ? routes : "index.html");//if route is empty assign home.html to page to ajax load the default content.
+
+      console.log(page);
+      var routes = page.substring(0, page.lastIndexOf('.'));//remove file extension that shows up in the url bar.
+      console.log(routes);
+
+      console.log("Ajax loaded: " + page);
+
+      $.get(page, function (pageContent) {//return selected page content trough ajax.
+         $("#container").html(pageContent);//load content into main div
+         _load_default();
+         _load_scripts();
+      });
+      window.history.pushState(null, null, routes);//assign new url to address bar and add page in browser history without reloading the page.
+
+   });
+}
+_load_default();
+
+
 
 //when window's history changes fire this event.
 //e.g When user goes back or forward in a session browser.
- $(window).on('popstate', function (){
+$(window).on('popstate', function () {
 
-  var url = window.location.href;//get page url from address bar.
-  var routes = url.substring(url.lastIndexOf('/')+1);//return page route from url.
-  var page = routes != '' ? url+".html" : "home.html" ;//if route is empty assign home.html to page to ajax load the default content.
+   var url = window.location.href;//get page url from address bar.
+   var routes = url.substring(url.lastIndexOf('/') + 1);//return page route from url.
+   console.log(routes);
+   var page = 'part/' + (routes != '' ? routes : "index.html");//if route is empty assign home.html to page to ajax load the default content.
 
-  console.log(page);
+   console.log(page);
 
-  $.get(page, function (pageContent) {//return selected page content trough ajax.
-   $("#container").html(pageContent);//load content into main div
-  });
+
+   $.get(page, function (pageContent) {//return selected page content trough ajax.
+      $("#container").html(pageContent);//load content into main div
+      _load_scripts();
+   });
 });
 
 
@@ -48,15 +87,19 @@ $(document).on('click','[data-link]', function (e) {
 //.htaccess for apache or any other web server edit file has to be edited 
 //to ensure that index.html file loads even if url in browser has been changed to maintain SPA system. 
 //e.g When user reloads the page or enters or paste a url with the same domain name but different path in the browser.
- $(window).on('load', function (){
+$(window).on('load', function () {
 
-  var url = window.location.href;//get page url from address bar.
-  var routes = url.substring(url.lastIndexOf('/')+1);//return page route from url.
-  var page = routes != '' ? url+".html" : "home.html" ;//if route is empty assign home.html to page to ajax load the default content.
+   var url = window.location.href;//get page url from address bar.
+   var routes = url.substring(url.lastIndexOf('/') + 1);//return page route from url.
+   console.log(routes);
 
-  console.log(page);
+   var page = 'part/' + (routes != '' ? routes : "index.html");//if route is empty assign home.html to page to ajax load the default content.
 
-  $.get(page, function (pageContent) {//return selected page content trough ajax.
-   $("#container").html(pageContent);//load content into main div
-  });
+   console.log(page);
+
+   $.get(page, function (pageContent) {//return selected page content trough ajax.
+      $("#container").html(pageContent);//load content into main div
+      _load_scripts();
+   });
+
 });
